696a55f11e9492fa821be96c1ee4a4f5b9063326
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2022-08-07 18:11:24 +0800

    Update replica_OnBF.cc

--- replication/vr/replica_OnBF.cc
+++ replication/vr/replica_OnBF.cc
@@ -1,5 +1,13 @@
 //replica logic on BF should be as lightweight as possible. All the slow path logic should be executed on Node10
 //We want to offload the extra tasks of a leader replica to BF, for example Send N Prepare messgare && Receive/Process N PrepareOK)
+/*
+ * The RDMA server side part of code. 
+ *
+ * Author: Animesh Trivedi 
+ *         atrivedi@apache.org 
+ *
+ * TODO: Cleanup previously allocated resources in case of an error condition
+ */
 #include "common/replica.h"
 #include "replication/vr/replica.h"
 
@@ -21,6 +29,28 @@ namespace vr {
 
 using namespace proto;
 
+/* These are the RDMA resources needed to setup an RDMA connection */
+/* Event channel, where connection management (cm) related events are relayed */
+static struct rdma_event_channel *cm_event_channel = NULL;
+static struct rdma_cm_id *cm_server_id = NULL, *cm_client_id = NULL;
+static struct ibv_pd *pd = NULL;
+static struct ibv_comp_channel *io_completion_channel = NULL;
+static struct ibv_cq *cq = NULL;
+static struct ibv_qp_init_attr qp_init_attr;
+static struct ibv_qp *client_qp = NULL;
+/* RDMA memory resources */
+static struct ibv_mr *client_metadata_mr = NULL, *server_buffer_mr = NULL, *server_metadata_mr = NULL;
+static struct rdma_buffer_attr client_metadata_attr, server_metadata_attr;
+static struct ibv_recv_wr client_recv_wr, *bad_client_recv_wr = NULL;
+static struct ibv_send_wr server_send_wr, *bad_server_send_wr = NULL;
+static struct ibv_sge client_recv_sge, server_send_sge;
+
+/* When we call this function cm_client_id must be set to a valid identifier.
+ * This is where, we prepare client connection before we accept it. This 
+ * mainly involve pre-posting a receive buffer to receive client side 
+ * RDMA credentials
+ */  
+  
 VRReplica::VRReplica(Configuration config, int myIdx,
                      bool initialize,
                      Transport *transport, int batchSize,
