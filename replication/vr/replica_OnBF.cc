b9c32f8ee68cdb0f95d06ad8ba71af7be93a1fb5
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2022-08-18 23:56:21 +0800

    add generatenonce()

--- replication/vr/replica_OnBF.cc
+++ replication/vr/replica_OnBF.cc
@@ -247,7 +247,15 @@ VRReplica::~VRReplica()
     }
     client_disconnect_and_clean();
 }
-
+		  
+uint64_t
+VRReplica::GenerateNonce() const
+{
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<uint64_t> dis;
+    return dis(gen);
+}
 //fast-path && non-leader
 bool
 VRReplica::AmLeader() const
