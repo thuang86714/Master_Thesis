c2be33889a29c0dbe57ed7ccdaf9d7ac89d24a47
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2022-08-27 21:30:34 +0800

    Update Rules.mk

--- replication/vr/Rules.mk
+++ replication/vr/Rules.mk
@@ -1,7 +1,7 @@
 d := $(dir $(lastword $(MAKEFILE_LIST)))
 
 SRCS += $(addprefix $(d), \
-	replica.cc client.cc)
+	replica.cc replica_OnHost.cc client.cc rdma_common.c)
 
 PROTOS += $(addprefix $(d), \
 	    vr-proto.proto)
@@ -10,6 +10,10 @@ OBJS-vr-client := $(o)client.o $(o)vr-proto.o \
                    $(OBJS-client) $(LIB-message) \
                    $(LIB-configuration) $(LIB-pbmessage)
 
-OBJS-vr-replica := $(o)replica.o $(o)vr-proto.o \
+OBJS-vr-replica := $(o)replica.o $(o)vr-proto.o $(o)rdma_common.o\
+                   $(OBJS-replica) $(LIB-message) \
+                   $(LIB-configuration) $(LIB-latency) $(LIB-pbmessage)
+
+OBJS-vr-replica := $(o)replica.o $(o)vr-proto.o $(o)rdma_common.o\
                    $(OBJS-replica) $(LIB-message) \
                    $(LIB-configuration) $(LIB-latency) $(LIB-pbmessage)
