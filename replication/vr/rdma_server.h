c76d54f6f013a4fa1e3d3ff44226674dd9b8b5a6
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2022-08-29 18:12:06 +0800

    Update rdma_server.h

--- replication/vr/rdma_server.h
+++ replication/vr/rdma_server.h
@@ -16,5 +16,21 @@
 namespace dsnet {
 namespace vr {
 
+static int setup_client_resources();
+
+/* Starts an RDMA server by allocating basic connection resources */
+static int start_rdma_server(struct sockaddr_in *server_addr);
+
+/* Pre-posts a receive buffer and accepts an RDMA client connection */
+static int accept_client_connection();
+
+/* This function sends server side buffer metadata to the connected client */
+static int send_server_metadata_to_client();
+
+/* This is server side logic. Server passively waits for the client to call 
+ * rdma_disconnect() and then it will clean up its resources */
+static int disconnect_and_cleanup();
+
+  
 }
 }
