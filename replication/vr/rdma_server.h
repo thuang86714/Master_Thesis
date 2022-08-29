58b8ad81811501335b69d6c445b1d2a4b9784a61
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2022-08-29 17:45:26 +0800

    Create rdma_server.h

---
+++ replication/vr/rdma_server.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <rdma/rdma_cma.h>
+#include <infiniband/verbs.h>
+#include "rdma_common.h"
+namespace dsnet {
+namespace vr {
+
+}
+}
